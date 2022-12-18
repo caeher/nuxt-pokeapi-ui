1247469852219d28726d504fc56d15cd52b77e32
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-12-18 13:14:15 -0600

    Updates

--- nuxt-pokeapi-ui/composables.49b3cfb1.js
+++
@@ -1,1 +0,0 @@
-import{C as r}from"./entry.8f5059aa.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
---
+++ spa/composables.4a02f881.js
@@ -0,0 +1,1 @@
+import{C as r}from"./entry.5df0d2b7.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
