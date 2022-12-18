07f7c7b9e97ed734b9aec8919d225bb8dc9ec288
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-12-18 14:26:56 -0600

    Updates

--- spa/composables.9297491c.js
+++
@@ -1,1 +0,0 @@
-import{C as r}from"./entry.cd8d5184.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
---
+++ spa/composables.b51ab587.js
@@ -0,0 +1,1 @@
+import{C as r}from"./entry.1a0b9265.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
