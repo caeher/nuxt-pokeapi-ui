3728792ba2e39b2477a9e73830ce6d04fbed2b22
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2022-12-18 15:22:28 -0600

    `style`: fixed text-color for hero h1

--- components/app/SimpleHero.vue
+++ components/app/SimpleHero.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="mx-auto mt-16 max-w-7xl px-4 sm:mt-24 sm:px-6">
     <div class="text-center">
-      <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+      <h1 class="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-300 sm:text-5xl md:text-6xl">
         <span class="block capitalize">Find your favorite pokemon.</span>
         <span class="block text-indigo-600 capitalize">catch them!</span>
       </h1>
